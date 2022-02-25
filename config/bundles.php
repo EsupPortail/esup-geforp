2c4842d491d439c138274b783c2c1f5a1e25e850
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2022-02-25 16:18:12 +0100

    ajout dependances pour generer des donnees dans la base

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     MBence\OpenTBSBundle\OpenTBSBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
