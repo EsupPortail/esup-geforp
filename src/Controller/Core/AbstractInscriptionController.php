ca4bbc3de0805d41a2f10c38449d6c3f25b0886a
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2022-05-24 16:56:23 +0200

    Ajout recherche inscriptions cote gestionnaire

--- src/Controller/Core/AbstractInscriptionController.php
+++ src/Controller/Core/AbstractInscriptionController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\Core;
 
+use App\Entity\Core\Term\Presencestatus;
+use App\Entity\Core\Term\Publictype;
 use App\Entity\Inscription;
+use App\Entity\Institution;
+use App\Entity\Organization;
 use App\Form\Type\BaseInscriptionType;
+use App\Repository\InscriptionSearchRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -30,7 +35,7 @@ abstract class AbstractInscriptionController extends AbstractController
      * @Route("/search", name="inscription.search", options={"expose"=true}, defaults={"_format" = "json"})
      * @Rest\View(serializerGroups={"Default", "inscription"}, serializerEnableMaxDepthChecks=true)
      */
-    public function searchAction(Request $request, ManagerRegistry $doctrine)
+    public function searchAction(Request $request, ManagerRegistry $doctrine, InscriptionSearchRepository $inscriptionSearchRepository)
     {
         /*
         $search = $this->get('sygefor_inscription.search');
@@ -43,13 +48,34 @@ abstract class AbstractInscriptionController extends AbstractController
 
         return $search->search();
         */
-        $inscriptions = $doctrine->getRepository(Inscription::class)->findAll();
+/*        $inscriptions = $doctrine->getRepository(Inscription::class)->findAll();
         $nbInscriptions  = count($inscriptions);
 
         $ret = array(
             'total' => $nbInscriptions,
             'pageSize' => 0,
             'items' => $inscriptions,
+        );
+        return $ret;*/
+
+        $keywords = $request->request->get('keywords', 'NO KEYWORDS');
+        $filters = $request->request->get('filters', 'NO FILTERS');
+        $query_filters = $request->request->get('query_filters', 'NO QUERY FILTERS');
+        $aggs = $request->request->get('aggs', 'NO AGGS');
+
+        // Recherche avec les filtres
+        $inscriptions = $inscriptionSearchRepository->getInscriptionsList($keywords, $filters);
+        $nbInscriptions  = count($inscriptions);
+
+        // Recherche pour aggs et query_filters
+        $tabAggs = array();
+        $tabAggs = $this->constructAggs($aggs, $keywords, $query_filters, $doctrine, $inscriptionSearchRepository);
+
+        $ret = array(
+            'total' => $nbInscriptions,
+            'pageSize' => 0,
+            'items' => $inscriptions,
+            'aggs' => $tabAggs
         );
         return $ret;
     }
@@ -155,5 +181,127 @@ abstract class AbstractInscriptionController extends AbstractController
         $inscription->setInscriptionstatus($defaultInscriptionStatus);
 
         return $inscription;
+    }
+
+    private function constructAggs($aggs, $keyword, $query_filters, $doctrine, $inscriptionRepository)
+    {
+        $tabAggs = array();
+
+        // CONSTRUCTION CENTRES
+        if(isset( $aggs['session.training.organization.name.source'])){
+            $allOrganizations = $doctrine->getRepository(Organization::class)->findAll();
+
+            $i = 0; $tabOrg = array();
+            //Pour chaque centre on teste la requête
+            foreach($allOrganizations as $organization){
+                $nbInscriptionsOrg = $inscriptionRepository->getNbInscriptions($query_filters, $keyword, $aggs, $organization->getName());
+                if ($nbInscriptionsOrg > 0) {
+                    $tabOrg[$i] = [ 'key' => $organization->getName(), 'doc_count' => $nbInscriptionsOrg];
+                    $i++;
+                }
+            }
+            $tabAggs['session.training.organization.name.source']['buckets'] = $tabOrg;
+        }
+
+        // CONSTRUCTION STATUT D'INSCRIPTION
+        if(isset( $aggs['inscriptionStatus.name.source'])){
+            $allInscStatus = $doctrine->getRepository(Inscriptionstatus::class)->findAll();
+
+            $i = 0; $tabStatInsc = array();
+            //Pour chaque statut d'inscription on teste la requête
+            foreach($allInscStatus as $status){
+                $nbInscriptionsStat = $inscriptionRepository->getNbInscriptions($query_filters, $keyword, $aggs, $status->getName());
+                if ($nbInscriptionsStat > 0) {
+                    $tabStatInsc[$i] = [ 'key' => $status->getName(), 'doc_count' => $nbInscriptionsStat];
+                    $i++;
+                }
+            }
+            $tabAggs['inscriptionStatus.name.source']['buckets'] = $tabStatInsc;
+        }
+
+        // CONSTRUCTION STATUT DE PRESENCE
+        if(isset( $aggs['presenceStatus.name.source'])){
+            $allPresStatus = $doctrine->getRepository(Presencestatus::class)->findAll();
+
+            $i = 0; $tabStatPres = array();
+            //Pour chaque statut de présence on teste la requête
+            foreach($allPresStatus as $status){
+                $nbPresStat = $inscriptionRepository->getNbInscriptions($query_filters, $keyword, $aggs, $status->getName());
+                if ($nbPresStat > 0) {
+                    $tabStatPres[$i] = [ 'key' => $status->getName(), 'doc_count' => $nbPresStat];
+                    $i++;
+                }
+            }
+            $tabAggs['presenceStatus.name.source']['buckets'] = $tabStatPres;
+        }
+
+        // CONSTRUCTION ETABLISSEMENT
+        if(isset( $aggs['institution.name.source'])){
+            $allInstitutions = $doctrine->getRepository(Institution::class)->findAll();
+
+            $i = 0; $tabInst = array();
+            //Pour chaque établissement on teste la requête
+            foreach($allInstitutions as $institution){
+                $nbInscriptionsInst= $inscriptionRepository->getNbInscriptions($query_filters, $keyword, $aggs, $institution->getName());
+                if ($nbInscriptionsInst > 0) {
+                    $tabInst[$i] = [ 'key' => $institution->getName(), 'doc_count' => $nbInscriptionsInst];
+                    $i++;
+                }
+            }
+            $tabAggs['institution.name.source']['buckets'] = $tabInst;
+        }
+
+        // CONSTRUCTION TYPE DE PERSONNEL
+        if(isset( $aggs['publicType.source'])){
+            $allPublicTypes = $doctrine->getRepository(Publictype::class)->findAll();
+
+            $i = 0; $tabPub = array();
+            //Pour chaque établissement on teste la requête
+            foreach($allPublicTypes as $publictype){
+                $nbInscriptionsPub= $inscriptionRepository->getNbInscriptions($query_filters, $keyword, $aggs, $publictype->getName());
+                if ($nbInscriptionsPub > 0) {
+                    $tabPub[$i] = [ 'key' => $publictype->getName(), 'doc_count' => $nbInscriptionsPub];
+                    $i++;
+                }
+            }
+            $tabAggs['publicType.source']['buckets'] = $tabPub;
+        }
+
+        // CONSTRUCTION ANNEE
+        if(isset( $aggs['session.year'])){
+            $curYear = date('Y');
+            $allYears = array();
+            for($i=2017; $i<=$curYear; $i++){
+                $allYears[] = $i;
+            }
+
+            $i = 0; $tabYear = array();
+            //Pour chaque établissement on teste la requête
+            foreach($allYears as $year){
+                $nbInscriptionsYear= $inscriptionRepository->getNbInscriptions($query_filters, $keyword, $aggs, $year);
+                if ($nbInscriptionsYear > 0) {
+                    $tabYear[$i] = [ 'key' => $year, 'doc_count' => $nbInscriptionsYear];
+                    $i++;
+                }
+            }
+            $tabAggs['session.year']['buckets'] = $tabYear;
+        }
+
+        // CONSTRUCTION SEMESTRE
+        if (isset($aggs['session.semester'])) {
+            $allSemesters = array(1, 2);
+            $i = 0; $tabSemesters = array();
+            //Pour chaque semestre on teste la requête
+            foreach($allSemesters as $semester){
+                $nbInsSem = $inscriptionRepository->getNbInscriptions($query_filters, $keyword, $aggs, $semester);
+                if ($nbInsSem > 0) {
+                    $tabSemesters[$i] = [ 'key' => $semester, 'doc_count' => $nbInsSem];
+                    $i++;
+                }
+            }
+            $tabAggs['session.semester']['buckets'] = $tabSemesters;
+        }
+
+        return $tabAggs;
     }
 }
---
+++ src/Repository/InscriptionSearchRepository.php
@@ -0,0 +1,280 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Core\Term\Inscriptionstatus;
+use App\Entity\Core\Term\Presencestatus;
+use App\Entity\Core\Term\Publictype;
+use App\Entity\Inscription;
+use App\Entity\Institution;
+use App\Entity\Session;
+use App\Entity\Internship;
+use App\Entity\Organization;
+use App\Entity\Trainee;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class InscriptionSearchRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Inscription::class);
+    }
+
+    public function getInscriptionsList($keyword, $filters)
+    {
+        $qb = $this->createQueryBuilder('i');
+        $qb
+            ->select(' i')
+            /* Keyword (recherche par mot clé) */
+            ->innerJoin(Organization::class, 'o')
+            ->innerJoin(Session::class, 's')
+            ->innerJoin(Internship::class, 'tr')
+            ->innerJoin(Inscriptionstatus::class, 'istatus')
+            ->innerJoin(Presencestatus::class, 'pstatus')
+            ->innerJoin(Institution::class, 'institution')
+            ->innerJoin(Trainee::class, 'trainee')
+            ->innerJoin(Publictype::class, 'publictype')
+
+            // FILTRE KEYWORD
+            ->where('trainee.firstname LIKE :keyword')
+            ->orWhere('trainee.lastname LIKE :keyword')
+            ->andWhere('i.trainee = trainee.id')
+            /* addcslashes empêchera des manipulations malveillantes éventuelles */
+            ->setParameter('keyword', '%' . addcslashes($keyword, '%_') . '%');
+
+
+        // FILTRE CENTRE
+        if (isset($filters['session.training.organization.name.source'])) {
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('s.training = tr.id')
+                ->andWhere('tr.organization = o.id')
+                ->andWhere('o.name in (:centers)')
+                ->setParameter('centers', $filters['session.training.organization.name.source']);
+        }
+
+        //FILTRE STATUT D'INSCRIPTION
+        if( isset($filters['inscriptionStatus.name.source'])) {
+            $qb
+                ->andWhere('i.inscriptionstatus = istatus.id')
+                ->andWhere('istatus.name = :status')
+                ->setParameter('status', $filters['inscriptionStatus.name.source']);
+        }
+
+        //FILTRE STATUT DE PRESENCE
+        if( isset($filters['presenceStatus.name.source'])) {
+            $qb
+                ->andWhere('i.presencestatus = pstatus.id')
+                ->andWhere('pstatus.name = :status')
+                ->setParameter('status', $filters['presenceStatus.name.source']);
+        }
+
+        //FILTRE ETABLISSEMENT
+        if( isset($filters['institution.name.source'])) {
+            $qb
+                ->andWhere('i.trainee = trainee.id')
+                ->andWhere('trainee.institution = institution.id')
+                ->andWhere('institution.name in (:institutions)')
+                ->setParameter('institutions', $filters['institution.name.source']);
+        }
+
+        //FILTRE CATEGORIE DE PERSONNEL
+        if( isset($filters['publicType.source'])) {
+            $qb
+                ->andWhere('i.trainee = trainee.id')
+                ->andWhere('trainee.publictype = publictype.id')
+                ->andWhere('publictype.name in (:publictypes)')
+                ->setParameter('publictypes', $filters['publicType.source']);
+        }
+
+        // FILTRE ANNEE
+        if (isset($filters['session.year'])) {
+            $qb
+                /* On récupère l'année du dateBegin (à l'aide d'une doctrine extension) */
+                ->andWhere('i.session = s.id')
+                ->andWhere('YEAR(s.datebegin) in (:years)')
+                ->setParameter('years', $filters['session.year']);
+        }
+
+        // FILTRE SEMESTRE
+        if( isset($filters['session.semester']) ) {
+            if ($filters['session.semester']  == 1) {
+                $monthFrom = 1; $monthTo = 6;
+            } else {
+                $monthFrom = 7; $monthTo = 12;
+            }
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('MONTH(s.datebegin) BETWEEN :monthFrom and :monthTo')
+                ->setParameter('monthFrom', $monthFrom)
+                ->setParameter('monthTo', $monthTo);
+        }
+
+        //FILTRE DATE
+        if( isset($filters['session.dateBegin']) ) {
+            /* La date envoyée par le formulaire en JS a un format : "dd/mm/yy - dd/mm/yy" il faut donc séparer les 2 dates */
+            $dates = explode('-', $filters["session.dateBegin"]);
+            /* on retire les caractères non utiles */
+            $from = str_replace('/','-', $dates[0]);
+            $to = str_replace('/', '-', $dates[1]);
+            /* on convertit au même format qu'en base de données */
+            $dateFrom = date('Y/m/d 00:00:00' ,strtotime($from));
+            $dateTo = date('Y/m/d 00:00:00',strtotime($to));
+
+            $qb
+                /* si la date de début d'une session est entre les 2 dates envoyées dans le formulaire */
+                ->andWhere('i.session = s.id')
+                ->andWhere("s.datebegin BETWEEN :dateFrom AND :dateTo")
+                ->setParameter('dateFrom', $dateFrom)
+                ->setParameter('dateTo', $dateTo);
+        }
+
+        $query = $qb->getQuery();
+
+        return $result = $query->getResult();
+    }
+
+    public function getNbInscriptions($query_filters, $keyword, $aggs, $name)
+    {
+        $qb = $this->createQueryBuilder('i');
+        $qb
+            ->select('i')
+            /* Keyword (recherche par mot clé) */
+            ->innerJoin(Organization::class, 'o')
+            ->innerJoin(Session::class, 's')
+            ->innerJoin(Internship::class, 'tr')
+            ->innerJoin(Inscriptionstatus::class, 'istatus')
+            ->innerJoin(Presencestatus::class, 'pstatus')
+            ->innerJoin(Institution::class, 'institution')
+            ->innerJoin(Trainee::class, 'trainee')
+            ->innerJoin(Publictype::class, 'publictype')
+
+            // FILTRE KEYWORD
+            ->where('trainee.firstname LIKE :keyword')
+            ->orWhere('trainee.lastname LIKE :keyword')
+            ->andWhere('i.trainee = trainee.id')
+            /* addcslashes empêchera des manipulations malveillantes éventuelles */
+            ->setParameter('keyword', '%' . addcslashes($keyword, '%_') . '%');
+
+
+        // FILTRE CENTRE
+        if(isset( $aggs['session.training.organization.name.source'])) {
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('s.training = tr.id')
+                ->andWhere('tr.organization = o.id')
+                ->andWhere('o.name = :center')
+                ->setParameter('center', $name);
+        } elseif (isset($query_filters['session.training.organization.name.source'])) {
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('s.training = tr.id')
+                ->andWhere('tr.organization = o.id')
+                ->andWhere('o.name in (:centers)')
+                ->setParameter('centers', $query_filters['session.training.organization.name.source']);
+        }
+
+        //FILTRE STATUT D'INSCRIPTION
+        if(isset( $aggs['inscriptionStatus.name.source'])) {
+            $qb
+                ->andWhere('i.inscriptionstatus = istatus.id')
+                ->andWhere('istatus.name = :status')
+                ->setParameter('status', $name);
+        } elseif( isset($query_filters['inscriptionStatus.name.source'])) {
+            $qb
+                ->andWhere('i.inscriptionstatus = istatus.id')
+                ->andWhere('istatus.name = :status')
+                ->setParameter('status', $query_filters['inscriptionStatus.name.source']);
+        }
+
+        //FILTRE STATUT DE PRESENCE
+        if( isset($aggs['presenceStatus.name.source'])) {
+            $qb
+                ->andWhere('i.presencestatus = pstatus.id')
+                ->andWhere('pstatus.name = :status')
+                ->setParameter('status', $name);
+        } elseif( isset($query_filters['presenceStatus.name.source'])) {
+            $qb
+                ->andWhere('i.presencestatus = pstatus.id')
+                ->andWhere('pstatus.name = :status')
+                ->setParameter('status', $query_filters['presenceStatus.name.source']);
+        }
+
+        //FILTRE ETABLISSEMENT
+        if( isset($aggs['institution.name.source'])) {
+            $qb
+                ->andWhere('i.trainee = trainee.id')
+                ->andWhere('trainee.institution = institution.id')
+                ->andWhere('institution.name = :institution')
+                ->setParameter('institution',$name);
+        }elseif( isset($query_filters['institution.name.source'])) {
+            $qb
+                ->andWhere('i.trainee = trainee.id')
+                ->andWhere('trainee.institution = institution.id')
+                ->andWhere('institution.name = :institution')
+                ->setParameter('institution', $query_filters['institution.name.source']);
+        }
+
+        //FILTRE CATEGORIE DE PERSONNEL
+        if( isset($aggs['publicType.source'])) {
+            $qb
+                ->andWhere('i.trainee = trainee.id')
+                ->andWhere('trainee.publictype = publictype.id')
+                ->andWhere('publictype.name = :publictype')
+                ->setParameter('publictype', $name);
+        }elseif( isset($query_filters['publicType.source'])) {
+            $qb
+                ->andWhere('i.trainee = trainee.id')
+                ->andWhere('trainee.publictype = publictype.id')
+                ->andWhere('publictype.name = :publictype')
+                ->setParameter('publictype', $query_filters['publicType.source']);
+        }
+
+        // FILTRE ANNEE
+        if (isset($aggs['session.year'])) {
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('YEAR(s.datebegin) = :year')
+                ->setParameter('year', $name);
+        } elseif (isset($query_filters['session.year'])) {
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('YEAR(s.datebegin) in (:years)')
+                ->setParameter('years', $query_filters['session.year']);
+        }
+
+        // FILTRE SEMESTRE
+        if (isset($aggs['session.semester'])) {
+            if ($name  == 1) {
+                $monthFrom = 1; $monthTo = 6;
+            } else {
+                $monthFrom = 7; $monthTo = 12;
+            }
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('MONTH(s.datebegin) BETWEEN :monthFrom and :monthTo')
+                ->setParameter('monthFrom', $monthFrom)
+                ->setParameter('monthTo', $monthTo);
+        } elseif( isset($query_filters['session.semester']) ) {
+            if ($query_filters['session.semester']  == 1) {
+                $monthFrom = 1; $monthTo = 6;
+            } else {
+                $monthFrom = 7; $monthTo = 12;
+            }
+            $qb
+                ->andWhere('i.session = s.id')
+                ->andWhere('MONTH(s.datebegin) BETWEEN :monthFrom and :monthTo')
+                ->setParameter('monthFrom', $monthFrom)
+                ->setParameter('monthTo', $monthTo);
+        }
+
+        // On compte le nb de sessions en résultat
+        $paginator = new \Doctrine\ORM\Tools\Pagination\Paginator($qb->getQuery());
+        $totalRows = count($paginator);
+        dump($totalRows);
+
+        return $totalRows;
+    }
+
+}
