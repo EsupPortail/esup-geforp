f989f887cf66eee5442f92faec10bff227f646f5
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2022-06-09 12:34:25 +0200

    Correction security user pour Shibboleth

--- src/Bundle/ShibbolethBundle/Security/User/ShibbolethUserProvider.php
+++ src/Bundle/ShibbolethBundle/Security/User/ShibbolethUserProvider.php
@@ -12,14 +12,22 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\HttpFoundation\RequestStack;
+use App\Bundle\ShibbolethBundle\Security\User\ShibbolethUserProviderinterface;
 
-class ShibbolethUserProvider implements UserProviderInterface
+
+class ShibbolethUserProvider implements ShibbolethUserProviderinterface
 {
 
     public function loadUserByUsername($login)
     {
         $roles = array();
-        return new ShibbolethUser($login, '', '',$roles);
+        return new ShibbolethUser($login, '', '', array(), $roles);
+    }
+
+    public function loadUser($credentials)
+    {
+        $roles = array();
+        return new ShibbolethUser($credentials['username'], '', '', $credentials, $roles);
     }
 
     public function refreshUser(UserInterface $user)
