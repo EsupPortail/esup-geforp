fbfeec99384dce9fbd8154c7256e5d2c100896c7
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2022-06-09 12:40:27 +0200

    Correction security user Shibboleth (recup des attributs)

--- src/Bundle/ShibbolethBundle/Security/User/ShibbolethUserProvider.php
+++ src/Bundle/ShibbolethBundle/Security/User/ShibbolethUserProvider.php
@@ -12,14 +12,21 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\HttpFoundation\RequestStack;
+use App\Bundle\ShibbolethBundle\Security\User\ShibbolethUserProviderInterface;
 
-class ShibbolethUserProvider implements UserProviderInterface
+class ShibbolethUserProvider implements ShibbolethUserProviderInterface
 {
 
     public function loadUserByUsername($login)
     {
         $roles = array();
         return new ShibbolethUser($login, '', '',$roles);
+    }
+
+    public function loadUser($credentials)
+    {
+        $roles = array();
+        return new ShibbolethUser($credentials['username'], '', '', $credentials, $roles);
     }
 
     public function refreshUser(UserInterface $user)
