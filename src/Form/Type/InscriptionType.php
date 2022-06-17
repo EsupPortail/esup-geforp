ed7e232b0929fe5577a88cb9fc218f9db11c45ec
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2022-06-17 09:57:02 +0200

    Correction formulaire inscription

--- src/Form/Type/InscriptionType.php
+++ src/Form/Type/InscriptionType.php
@@ -3,19 +3,18 @@
 namespace App\Form\Type;
 
 
-use App\Entity\Core\AbstractInscription;
-use Symfony\Component\Form\AbstractType;
+use App\Entity\Inscription;
+use App\Entity\Core\Term\ActionType;
+use App\Form\Type\EntityHiddenType;
+use App\Entity\Core\AbstractTrainee;
+use App\Entity\Core\AbstractSession;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Entity\Core\Term\ActionType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-/**
- * Class InscriptionType.
- */
 class InscriptionType extends AbstractType
 {
     /**
@@ -25,31 +24,39 @@ class InscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('trainee', EntityHiddenType::class, array(
+                'label' => 'Stagiaire',
+                'class' => AbstractTrainee::class
+            ))
+            ->add('session', EntityHiddenType::class, array(
+                'label' => 'Session',
+                'class' => AbstractSession::class
+            ))
             ->add('motivation', TextareaType::class, array(
                 'label' => 'Motivation',
-                'required' => false
+                'attr' => array('placeholder' => 'Expliquez les raisons pour lesquelles vous souhaitez vous inscrire à cette session.')
             ))
             ->add('actiontype', EntityType::class, array(
-            'label' => 'Type de formation',
-            'class' => ActionType::class
+                'label' => 'Type de formation',
+                'class' => ActionType::class
             ))
             ->add('dif', CheckboxType::class, array(
-            'label'    => 'Compte personnel de formation',
-            'required' => false,
+                'label' => 'Compte personnel de formation',
+                'required' => false
+            ))
+            ->add('authorization', CheckboxType::class, array(
+                'label' => 'Envoyer une demande d\'autorisation à mon supérieur hiérarchique',
+                'mapped' => false,
+                'required' => false,
+                'disabled' => true,
+                'attr' => array('checked'   => 'checked'),
             ));
-
-
-        parent::buildForm($builder, $options);
     }
 
-
-    /**
-     * @param OptionsResolver $resolver
-     */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => AbstractInscription::class,
+            'data_class' => Inscription::class,
         ));
     }
 }
