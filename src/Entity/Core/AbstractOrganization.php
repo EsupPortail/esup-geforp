3a92830450ec55c7734019671686b4bf8ae7807d
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2022-06-29 16:42:04 +0200

    Correction serializer Organization

--- src/Entity/Core/AbstractOrganization.php
+++ src/Entity/Core/AbstractOrganization.php
@@ -5,6 +5,9 @@ namespace App\Entity\Core;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Core\PersonTrait\CoordinatesTrait;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as Serializer;
 use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 
@@ -24,7 +27,232 @@ use Symfony\Component\Serializer\Annotation\MaxDepth;
  */
 abstract class AbstractOrganization
 {
-    use CoordinatesTrait;
+    /**
+     * @var boolean addressType
+     *
+     * @ORM\Column(name="address_type", type="integer", nullable=true)
+     * @Serializer\Exclude
+     */
+    protected $addresstype;
+
+    /**
+     * @var string address
+     *
+     * @ORM\Column(name="address", type="string", length=512, nullable=true)
+     * @Serializer\Groups({"api"})
+     */
+    protected $address;
+
+    /**
+     * @var string zip
+     *
+     * @ORM\Column(name="zip", type="string", length=32, nullable=true)
+     * @Serializer\Groups({"api"})
+     */
+    protected $zip;
+
+    /**
+     * @var string city
+     *
+     * @ORM\Column(name="city", type="string", length=128, nullable=true)
+     * @Serializer\Groups({"api"})
+     */
+    protected $city;
+
+    /**
+     * @var string
+     * @Assert\Email(message="Vous devez renseigner un email valide.")
+     * @ORM\Column(name="email", type="string", length=128, nullable=true)
+     * @Serializer\Groups({"api"})
+     */
+    protected $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone_number", type="string", length=255, nullable=true)
+     * @Serializer\Groups({"api"})
+     */
+    protected $phonenumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fax_number", type="string", length=255, nullable=true)
+     * @Serializer\Groups({"api"})
+     */
+    protected $faxnumber;
+
+    /**
+     * @var string
+     * @ORM\Column(name="website", type="string", length=512, nullable=true)
+     * @Serializer\Groups({"api"})
+     */
+    protected $website;
+
+    /**
+     * Copy coordinates from another entity.
+     *
+     * @param CoordinatesTrait $entity
+     * @param bool             $force  override existing data
+     */
+    public function copyCoordinates($entity, $force = true)
+    {
+        $propertyAccessor = new PropertyAccessor();
+        foreach (array('addresstype', 'address', 'zip', 'city', 'email', 'phonenumber', 'faxnumber', 'website') as $property) {
+            $thisValue = $propertyAccessor->getValue($this, $property);
+            if ($force || ! $thisValue) {
+                $propertyAccessor->setValue($this, $property, $propertyAccessor->getValue($entity, $property));
+            }
+        }
+    }
+
+
+    /*
+     * @param boolean $addressType
+     */
+    public function setAddresstype($addressType)
+    {
+        $this->addresstype = $addressType;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getAddresstype()
+    {
+        return $this->addresstype;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $zip
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param string $phoneNumber
+     */
+    public function setPhonenumber($phoneNumber)
+    {
+        $this->phonenumber = $phoneNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhonenumber()
+    {
+        return $this->phonenumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFaxnumber()
+    {
+        return $this->faxnumber;
+    }
+
+    /**
+     * @param string $faxNumber
+     */
+    public function setFaxnumber($faxNumber)
+    {
+        $this->faxnumber = $faxNumber;
+    }
+
+    /**
+     * @param string $website
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * Return the full address.
+     *
+     * @return string
+     */
+    public function getFullAddress()
+    {
+        $lines = array();
+        if ($this->getAddress()) {
+            $lines[] = $this->getAddress();
+        }
+        if ($this->getCity()) {
+            $lines[] = ($this->getZip() ? $this->getZip() . ' ' : '') . $this->getCity();
+        }
+
+        return implode("\n", $lines);
+    }
 
     /**
      * @var int
@@ -32,6 +260,7 @@ abstract class AbstractOrganization
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @Serializer\Groups({"Default", "api"})
      */
     protected $id;
 
@@ -39,6 +268,7 @@ abstract class AbstractOrganization
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255)
+     * @Serializer\Groups({"Default", "api"})
      */
     protected $name;
 
@@ -46,19 +276,21 @@ abstract class AbstractOrganization
      * @var string
      *
      * @ORM\Column(name="code", type="string", length=32)
+     * @Serializer\Groups({"Default", "api"})
      */
     protected $code;
 
     /**
      * @var ArrayCollection
      * @ORM\OneToMany(targetEntity="User", mappedBy="organization", cascade={"persist", "merge"})
-     * @MaxDepth(2)
+     * @Serializer\Exclude
      */
     private $users;
 
     /**
      * @var bool
      * @ORM\Column(name="trainee_registrable", type="boolean")
+     * @Serializer\Groups({"api"})
      */
     protected $traineeRegistrable = true;
 
@@ -66,6 +298,7 @@ abstract class AbstractOrganization
      * @var AbstractInstitution
      * @ORM\ManyToOne(targetEntity="App\Entity\Core\AbstractInstitution")
      * @ORM\JoinColumn(nullable=true, onDelete="SET NULL")
+     * @Serializer\Groups({"api"})
      */
     private $institution;
 
