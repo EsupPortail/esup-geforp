0297b6e2ab1c58f28b31e4abe27bf1fd10b9d387
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2022-09-09 11:06:06 +0200

    Modif pour authentification Shibboleth admin

--- src/Bundle/AdminShibbolethBundle/Security/User/ShibbolethUserProvider.php
+++ src/Bundle/AdminShibbolethBundle/Security/User/ShibbolethUserProvider.php
@@ -13,25 +13,38 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\HttpFoundation\RequestStack;
 use App\Bundle\AdminShibbolethBundle\Security\User\ShibbolethUserProviderInterface;
+use App\Entity\Core\User;
+use Doctrine\ORM\EntityRepository;
 
 class ShibbolethUserProvider implements ShibbolethUserProviderInterface
 {
 
     public function loadUserByUsername($login)
     {
-        $roles = array();
-        return new ShibbolethUser($login, '', '', array(), $roles);
+/*        $roles = array();
+        return new ShibbolethUser($login, '', '', array(), $roles);*/
+
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :username')
+            ->setParameter('username', $login)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
     public function loadUser($credentials)
     {
-        $roles = array();
-        return new ShibbolethUser($credentials['username'], '', '', $credentials, $roles);
+/*        $roles = array();
+        return new ShibbolethUser($credentials['username'], '', '', $credentials, $roles);*/
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :username')
+            ->setParameter('username', $credentials['username'])
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
     public function refreshUser(UserInterface $user)
     {
-        if (!$user instanceof ShibbolethUser) {
+        if (!$user instanceof User) {
             throw new UnsupportedUserException(
                 sprintf('Instances of "%s" are not supported.', get_class($user))
             );
@@ -42,6 +55,6 @@ class ShibbolethUserProvider implements ShibbolethUserProviderInterface
 
     public function supportsClass($class)
     {
-        return ShibbolethUser::class === $class;
+        return User::class === $class;
     }
 }
--- src/Entity/Core/User.php
+++ src/Entity/Core/User.php
@@ -140,7 +140,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_SHIB_USER';
 
         return array_unique($roles);
     }
