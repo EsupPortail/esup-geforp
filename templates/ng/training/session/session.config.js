c781188acfb42df8ba5c7a73886c3934851fbaee
Peggy Fernandez <peggy.fernandez-blanco@univ-amu.fr>
2025-06-12 11:04:14 +0200

    Modification tri date fin sur les dernieres sessions affichees sur dashboard

--- templates/ng/training/session/session.config.js
+++ templates/ng/training/session/session.config.js
@@ -307,7 +307,7 @@ sygeforApp.config(["$listStateProvider", "$dialogProvider", "$widgetProvider", f
                 state: 'session.table',
                 title: 'Dernières sessions terminées',
                 size: 10,
-                sorts: {'datebegin': 'desc'},
+                sorts: {'dateend': 'desc'},
                 filters: {
                     'training.organization.name.source': $user.organization.name,
                     'dateend': moment().subtract('years', 1).format('DD/MM/YYYY') + ' - ' + moment().subtract('days', 1).format('DD/MM/YYYY')
